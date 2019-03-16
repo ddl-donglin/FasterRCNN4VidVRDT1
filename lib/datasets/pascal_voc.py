81662037412825f7fa71ecc270dd4e156dd0c597
daivd <daviddl997004866@gmail.com>
2019-03-16 16:02:36 +0800

    fix bug

--- lib/datasets/pascal_voc.py
+++ lib/datasets/pascal_voc.py
@@ -31,7 +31,7 @@ class pascal_voc(imdb):
                          'cow', 'diningtable', 'dog', 'horse',
                          'motorbike', 'person', 'pottedplant',
                          'sheep', 'sofa', 'train', 'tvmonitor')
-        self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
+        self._class_to_ind = dict(zip(self.classes, range(self.num_classes)))
         self._image_ext = '.jpg'
         self._image_index = self._load_image_set_index()
         # Default to roidb handler

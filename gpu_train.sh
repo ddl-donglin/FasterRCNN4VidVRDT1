dbcf79a54cfdd7fb8cde0012863a1d3788c077a3
daivd <daviddl997004866@gmail.com>
2019-03-16 18:35:31 +0800

    low coupling gpu_train.sh DATASET NETWORK

--- gpu_train.sh
+++ gpu_train.sh
@@ -6,11 +6,16 @@ WORKER_NUMBER=2
 LEARNING_RATE=0.01
 SAVE_DIR='data/output'
 DATASET=$1
+NETWORK=$2
 
+# vidor, pascal_voc
 echo ${DATASET}
 
+# vgg16, res101
+echo ${NETWORK}
+
 CUDA_VISIBLE_DEVICES=$GPU_ID python trainval_net.py \
-                   --dataset ${DATASET} --net vgg16 \
+                   --dataset ${DATASET} --net ${NETWORK} \
                    --bs $BATCH_SIZE --nw $WORKER_NUMBER \
                    --lr $LEARNING_RATE \
                    --save_dir $SAVE_DIR \

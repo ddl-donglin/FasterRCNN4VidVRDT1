9cc4ebb75d894a4673ab0e8489be8a4804c8bc3e
daivd <daviddl997004866@gmail.com>
2019-04-10 22:13:48 +0800

    modify test net...

--- test_net.py
+++ test_net.py
@@ -143,7 +143,7 @@ if __name__ == '__main__':
     if args.net == 'vgg16':
         fasterRCNN = vgg16(imdb.classes, pretrained=False, class_agnostic=args.class_agnostic)
     elif args.net == 'res101':
-        fasterRCNN = resnet(imdb.classes, 101, pretrained=False, class_agnostic=args.class_agnostic)
+        fasterRCNN = resnet(imdb.classes, 101, pretrained=True, class_agnostic=args.class_agnostic)
     elif args.net == 'res50':
         fasterRCNN = resnet(imdb.classes, 50, pretrained=False, class_agnostic=args.class_agnostic)
     elif args.net == 'res152':

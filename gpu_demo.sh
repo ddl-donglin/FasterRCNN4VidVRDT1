d00937070f2bab91bd2f215013ed0d03d3356729
davidddl <daviddl997004866@gmail.com>
2019-05-22 21:51:37 +0800

    modify demo

--- gpu_demo.sh
+++ gpu_demo.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 SESSION=1
-EPOCH=6
-CHECKPOINT=416
+EPOCH=4
+CHECKPOINT=283995
+path_to_model_directory='/storage/dldi/PyProjects/FasterRCNN4VidVRDT1/data/output/res101/vidor'
 
-python demo.py --net vgg16 \
+python demo.py --net res101 \
                --checksession ${SESSION} --checkepoch ${EPOCH} --checkpoint ${CHECKPOINT} \
-               --cuda --load_dir path/to/model/directoy
+               --cuda --load_dir ${path_to_model_directory}
